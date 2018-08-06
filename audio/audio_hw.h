0992c5bbcc786a6c4e70964279714cb19f87b6b8
McFy49 <g.matthieu49@gmail.com>
2018-08-06 14:20:58 +0000

    Revert "audio: Add flag to opt in/out amplifier support"
    This reverts commit a61d0e93e2ebdeecca1845cee80ad752486544a1.

--- audio/audio_hw.h
+++ audio/audio_hw.h
@@ -21,10 +21,7 @@
 
 #include <cutils/list.h>
 #include <hardware/audio.h>
-
-#ifdef DEVICE_HAS_AMPLIFIER_SUPPORT
 #include <hardware/audio_amplifier.h>
-#endif
 
 #include <tinyalsa/asoundlib.h>
 #include <tinycompress/tinycompress.h>
@@ -416,10 +413,7 @@ struct audio_device {
 #endif
 
     pthread_mutex_t         lock_inputs; /* see note below on mutex acquisition order */
-
-#ifdef DEVICE_HAS_AMPLIFIER_SUPPORT
     amplifier_device_t      *amp;
-#endif
 };
 
 /*
